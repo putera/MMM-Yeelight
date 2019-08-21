dd71804269280b8da03fbd27c7e691f0805d1c48
Zulkifli Mohamed <mr.putera@gmail.com>
2019-08-21 20:37:03 +0800

    Add translation & support for TelegramBot

--- MMM-Yeelight.js
+++ MMM-Yeelight.js
@@ -15,6 +15,62 @@ Module.register("MMM-Yeelight", {
 		lights: []
 	},
 
+	getTranslations: function() {
+		return {
+			"ms-my": "translations/ms-my.json",
+			"en": "translations/en.json"
+		}
+	},
+
+	getCommands: function(reg) {
+		if (reg.constructor.name == 'TelegramBotCommandRegister')
+		{
+			reg.add({
+				command: 'light',
+				description : this.translate("TELBOT_LIGHT"),
+				callback : 'cmd_light',
+				args_pattern : [/^on|off/i],
+				args_mapping : ['onoff']
+			});
+		}
+	},
+
+	cmd_light: function (command, handler) {
+		var text = ""; var moduleFound = 0;
+
+		// Check if MMM-Yeelight is installed on your MagicMirror
+		MM.getModules().enumerate((m) => {
+			if (m.name !== 'MMM-Yeelight') {
+				moduleFound = 1;
+			}
+		});
+
+		if (moduleFound == 0)
+		{
+			text = this.translate("TELBOT_LIGHT_NOT_INSTALLED");
+		}
+		else
+		{
+			if (handler.args !== null) {
+				var stat = handler.args['onoff'].toLowerCase();
+				if (stat == 'on')
+				{
+					this.sendNotification('TURN_ON_LIGHT', {timer: 2000});
+					text = this.translate("TELBOT_LIGHT_TURNING_ON");
+				}
+				else if (stat == 'off')
+				{
+					this.sendNotification('TURN_OFF_LIGHT', {timer: 2000});
+					text = this.translate("TELBOT_LIGHT_TURNING_OFF");
+				}
+			} else {
+				text = this.translate("TELBOT_LIGHT_SPEFICY");
+			}
+		}
+
+		handler.reply("TEXT", text, {parse_mode:'Markdown'});
+	},
+
 	notificationReceived: function(notification, payload, sender) {
 		var self = this;
 
